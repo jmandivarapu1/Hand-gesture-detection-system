0c81515a2c52644f02e44917477552939da8b0bf
nogbazg <nogbazghi@gmail.com>
2021-11-28 18:13:13 -0500

    more features

--- gesture_gui.py
+++ gesture_gui.py
@@ -1,11 +1,10 @@
-#Import tkinter library
 from tkinter import *
-#Create an instance of tkinter frame
+
+#tkinter frame instance
 win= Tk()
 win.title("IOT Project GUI")
-win.geometry("325x570")
+win.geometry("360x570")
 
-#Define a function to highlight the text
 def add_highlight():
    global text
    text.tag_add("selected", "1.00",END)
@@ -54,25 +53,50 @@ def undo():
       text.insert(lastDeletedStartIndex, lastDeletedText)
    print("undid change")
 
+def moveToBeginningOfLine():
+   global text
+   text.mark_set("insert", "1.0")
+   text.see("insert")
+   print("moved cursor to beginning of line")
+
+def moveToEndOfLine():
+   global text
+   text.mark_set("insert", "1.11")
+   text.see("insert")
+   print("moved cursor to end of line")
+
+def get_location(event):
+   global text
+   global currentCursorLocationOnClick
+   currentCursorLocationOnClick = text.index(CURRENT)
+   print("location of click: ", currentCursorLocationOnClick)
+
 indexStart = ""
 indexEnd = ""
 copiedText = ""
 lastDeletedText = ""
 lastDeletedStartIndex = ""
 lastDeletedEndIndex = ""
-
+currentCursorLocationOnClick = ""
 #Text Field
 text= Text(win)
 text.insert(INSERT, "Testing GUI")
+text.bind('<Button-1>', get_location)
 text.pack()
-#Create a Button to highlight text
+
+#highlight text
 Button(win, text= "Add highlight", command=add_highlight).pack()
-#Create a Button to remove highlight
+#remove highlight
 Button(win, text= "remove highlight", command=remove_Highlight).pack()
-#Create a Button to delete highlight text
+#delete highlight text
 Button(win, text= "delete text", command=deleteText).pack()
-#Create a Button to copy highlighted text
+#copy highlighted text
 Button(win, text= "copy text", command=copyText).pack()
-#Create a Button to undo delete
+#undo delete
 Button(win, text= "undo", command=undo).pack()
+#move to beginning of line
+Button(win, text= "move to beginning of line", command=moveToBeginningOfLine).pack()
+#move to end of line
+Button(win, text= "move to end of line", command=moveToEndOfLine).pack()
+
 win.mainloop()
